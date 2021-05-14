f41cf0249deeebdb3217881abfbcaea20cb8b740
mkhorin <maksimovichu@gmail.com>
2021-05-15 09:59:11 +1000

    Migration to Bootstrap 5

--- module/front/web/src/CommentList.js
+++ module/front/web/src/CommentList.js
@@ -80,7 +80,7 @@ Front.CommentList = class CommentList extends Front.Loadable {
     onCreate () {
         this.getHandler('Form').trigger('form:clear');
         this.$newError.addClass('hidden');
-        this.$new.modal();
+        this.modal = Jam.showModal(this.$new);
     }
 
     onSend () {
@@ -99,7 +99,7 @@ Front.CommentList = class CommentList extends Front.Loadable {
     }
 
     onSendDone (data) {
-        this.$new.modal('hide');
+        this.modal.hide();
         this.load();
     }
 
